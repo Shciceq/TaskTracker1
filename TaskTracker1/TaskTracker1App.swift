0e04c6cbe46f549160c362222ff8e92a359861f3
Shciceq <147521@scwsd.us>
2025-05-14 10:19:02 -0400

    Initial Commit

---
+++ TaskTracker1/TaskTracker1App.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTracker1App.swift
+//  TaskTracker1
+//
+//  Created by Bohdan Harbuziuk on 5/14/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskTracker1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TaskTracker1Tests/TaskTracker1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  TaskTracker1Tests.swift
+//  TaskTracker1Tests
+//
+//  Created by Bohdan Harbuziuk on 5/14/25.
+//
+
+import Testing
+@testable import TaskTracker1
+
+struct TaskTracker1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
